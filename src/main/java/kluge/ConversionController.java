f48e2fa385d8680468b995b0ea38dc51a8886b7e
Roland Kluge <roland.kluge@yodle.com>
2018-05-06 13:58:44 -0400

    both endpoints are returning the tracking pixel

--- src/main/java/kluge/ConversionController.java
+++ src/main/java/kluge/ConversionController.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,13 +31,13 @@ public class ConversionController {
         pixelBytes = Files.readAllBytes(new File(PIXEL_PATH).toPath());
     }
 
-    @RequestMapping(value="/image/track", method=RequestMethod.GET)
-    public ResponseEntity<String> track(@RequestParam String cId,
+    @RequestMapping(value="/image/track", method=RequestMethod.GET, produces=MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> track(@RequestParam String cId,
                                         @RequestParam String aId,
                                         HttpServletRequest request,
                                         HttpServletResponse response) {
 
-        // Testing work with cookies
+        // Create the cookie, using a structure name and value scheme
         String cookieName = COOKIE_PREFIX + DELIM + cId;
         String cookieValue = cId + DELIM + aId;
         Cookie c = new Cookie(cookieName, cookieValue);
@@ -47,31 +48,31 @@ public class ConversionController {
         // the user session, the headers, the cookies
         // This is important for tracking impressions/ eyeballs
 
-        return new ResponseEntity<>("Hello", HttpStatus.OK);
+        return new ResponseEntity<>(pixelBytes, HttpStatus.OK);
     }
 
-    @RequestMapping(value="/image/convert", method=RequestMethod.GET)
-    public ResponseEntity<String> convert(@RequestParam String cId,
+    @RequestMapping(value="/image/convert", method=RequestMethod.GET, produces=MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> convert(@RequestParam String cId,
                                           HttpServletRequest request,
                                           HttpServletResponse response) {
-        final String campaignCookieName = COOKIE_PREFIX + DELIM + cId;
+        final String cIdCookieName = COOKIE_PREFIX + DELIM + cId;
 
         Cookie[] cookies = request.getCookies();
         for(Cookie c : cookies) {
             // NOTE: Based on how we structure the Cookie name we can see the other campaign/ads that this user had viewed
             // record that information and send it back for further data science discovery
             String cookieName = c.getName();
-            if(campaignCookieName.equals(cookieName)) {
+            if(cIdCookieName.equals(cookieName)) {
                 String cookieValue = c.getValue();
-                String[] tokens = cookieValue.split("-");
-                String cookieCampaignId = tokens[0];
+                String[] tokens = cookieValue.split(DELIM);
+                String cookieCId = tokens[0];
                 String aId = tokens[1];
 
                 // TODO:
                 // record which campaign and ad combo created this event
-                System.out.println("You saw my ad, this is my conversion");
+                System.out.println("You saw my ad, this is my conversion cid="+cId+" aId="aId);
             }
         }
-        return new ResponseEntity<>("Convert", HttpStatus.OK);
+        return new ResponseEntity<>(pixelBytes, HttpStatus.OK);
     }
 }
